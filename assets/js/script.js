2d50bf5681811d20f9ea87edb275182a2edd5637
Caleb Korson <caleb@Calebs-Air.attlocal.net>
2022-08-28 13:54:26 -0400

    added class based on current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,7 @@ let currentDay = $('#currentDay')
 currentDay.text(moment().format('dddd, MMMM Do'))
 
 
+
 function createLines() {
     for (i=0; i<10; i++) {
         scheduleLines.push($('<section>'))
@@ -16,10 +17,25 @@ function createLines() {
         saveBlock.push($('<h2>'))
         scheduleLines[i].addClass('row')
         timeBlock[i].addClass('col-1 hour time-block')
-        taskBlock[i].addClass('col-10 past description')
+        taskBlock[i].addClass('col-10 description')
         saveBlock[i].addClass('col-1 saveBtn')
         // taskBlock[i].text('do stuff')
         saveBlock[i].text('Save')
+    }
+}
+
+function formatTask(i,hour) {
+    // console.log(hour)
+    // console.log(moment().format('hA'))
+    // console.log(moment().hour(10).format('hA') < moment().format('hA'))
+    if (hour < moment().format('hA')) {
+        taskBlock[i].addClass('past')
+    }
+    else if (hour == moment().format('hA')) {
+        taskBlock[i].addClass('present')
+    }
+    else {
+        taskBlock[i].addClass('future')
     }
 }
 
@@ -27,11 +43,15 @@ function createCalendar() {
     createLines()
     let hour 
     for (i=0; i<10; i++) {
-        hour = moment().hour(i+9).format('hA')
+        hour = moment().hour(i+6).format('hA')
         timeBlock[i].text(hour)
+        formatTask(i,hour)
         scheduleLines[i].append(timeBlock[i],taskBlock[i],saveBlock[i])
         schedule.append(scheduleLines[i])
     }
 }
 
+// console.log(moment(1,'H') < moment(12,'H'))
+// console.log(moment(11,'H').format('H'))
+// console.log(moment(12,'H').format('H'))
 createCalendar()
